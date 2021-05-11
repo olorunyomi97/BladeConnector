9eb5f17481ed78ad0f358854f7434c7208e7f183
olorunyomi97 <olorunyomi97@gmail.com>
2021-05-11 15:07:58 +0100

    Login Auth

--- routes/api/users.js
+++ routes/api/users.js
@@ -49,4 +49,29 @@ router.post('/register', (req, res) => {
     });
 });
 
+// @route       GET api/users/login//
+// @description login user// returning the jwt token
+// @access      public
+router.post('/login', (req, res) => {
+    const email = req.body.email;
+    const password = req.body.password;
+
+    // find user by email //
+    User.findOne({ email })
+    .then(user => {
+        // check for user //
+        if(!user) {
+            return res.status(404).json({ email: "user email not found" });
+        } 
+        // check password //
+        bcrypt.compare(password, user.password)
+        .then(isMatch => {
+            if(isMatch) {
+                res.json({ msg: 'Success' })
+            } else {
+                return res.status(400).json({ password: "incorrect password"});
+            }
+        })
+    })
+})
 module.exports = router;
