76d9af0be819c3d9dbb7307e26e4dc422ab89362
olorunyomi97 <olorunyomi97@gmail.com>
2021-05-11 15:57:12 +0100

    Jwt Token

--- config/keys.js
+++ config/keys.js
@@ -1,3 +1,4 @@
 module.exports = {
-    mongoURI: 'mongodb+srv://Abimbola:soodamnawesome@bladecluster.v1r2h.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
+    mongoURI: 'mongodb+srv://Abimbola:soodamnawesome@bladecluster.v1r2h.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',
+    secretOrkey: 'secret'
 }
