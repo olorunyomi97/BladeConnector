fb1cff41e581708bcfbf1b8e0683cfe5666bf2c6
olorunyomi97 <olorunyomi97@gmail.com>
2021-05-17 22:47:00 +0100

    like & unlike features

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -77,4 +77,26 @@ router.delete('/:id', passport.authenticate('jwt', { session: false }), (req, re
     })
 });
 
+
+// @route POST api/post/like/:id //
+// @description Like Blog Posts //
+// @access private //
+router.post('/like/:id', passport.authenticate('jwt', { session: false }), (req, res)=>{
+    Profile.findOne({ user: req.user.id })
+    .then(profile => {
+        Post.findById(req.params.id)
+        .then(post => {
+            // check if the post has been liked //
+           if(post.likes.filter(like => like.user.toString() === req.user.id).length > 0) {
+            return res.status(400).json({ alreadyliked: 'User already liked post' });
+           }
+           // Add user id to likes array
+           post.likes.unshift({ user: req.user.id });
+           post.save().
+           then(post => res.json(post));
+        })
+        .catch(err => res.status(404).json({ postnotfound : 'Post not found' }));
+    });
+});
+
 module.exports = router;
