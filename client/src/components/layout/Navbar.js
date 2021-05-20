15cb1a0ad3bf599358f5b189311acccea00ce370
olorunyomi97 <olorunyomi97@gmail.com>
2021-05-20 21:22:19 +0100

    React Router Setup

--- client/src/components/layout/Navbar.js
+++ client/src/components/layout/Navbar.js
@@ -1,4 +1,5 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 
 class Navbar extends Component {
     render() {
@@ -6,28 +7,27 @@ class Navbar extends Component {
             <div>
                 <nav className="navbar navbar-expand-sm navbar-dark bg-dark mb-4">
                     <div className="container">
-                    <a className="navbar-brand" href="landing.html">BladeConnector</a>
-                    <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#mobile-nav">
-                        <span className="navbar-toggler-icon"></span>
-                    </button>
+                        <Link className="navbar-brand" to="/">BladeConnector</Link>
+                        <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#mobile-nav">
+                            <span className="navbar-toggler-icon"></span>
+                        </button>
 
-                    <div className="collapse navbar-collapse" id="mobile-nav">
-                        <ul className="navbar-nav mr-auto">
-                        <li className="nav-item">
-                            <a className="nav-link" href="profiles.html"> Developers
-                            </a>
-                        </li>
-                        </ul>
+                        <div className="collapse navbar-collapse" id="mobile-nav">
+                            <ul className="navbar-nav mr-auto">
+                                <li className="nav-item">
+                                    <a className="nav-link" href="profiles.html"> {' '}Developers</a>
+                                </li>
+                            </ul>
 
-                        <ul className="navbar-nav ml-auto">
-                        <li className="nav-item">
-                            <a className="nav-link" href="register.html">Sign Up</a>
-                        </li>
-                        <li className="nav-item">
-                            <a className="nav-link" href="login.html">Login</a>
-                        </li>
-                        </ul>
-                    </div>
+                            <ul className="navbar-nav ml-auto">
+                                <li className="nav-item">
+                                    <Link className="nav-link" to="/register">Sign Up</Link>
+                                </li>
+                                <li className="nav-item">
+                                    <Link className="nav-link" to="/login">Login</Link>
+                                </li>
+                            </ul>
+                        </div>
                     </div>
                 </nav>
             </div>
